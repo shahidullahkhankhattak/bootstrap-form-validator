f310523a4e052ac4e0a232ecd51887f3ea036733
Shahid Ullah Khan <shahid.ktk9@gmail.com>
2019-11-04 05:07:33 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,98 @@
+(function($){
+	"use strict"
+	
+	let isFormValid = true;
+	
+	let messages = {
+		required: 'This field is required.',
+		email: 'Please enter a valid email',
+	};
+
+	function addError(elem, message){
+		elem.parent().removeClass("has-error");
+		elem.parent().find(".help-block").remove();
+		let error = '<span class="help-block">' +message+ '</span>';
+		elem.parent().addClass("has-error");
+		elem.parent().append(error);
+		isFormValid = false;
+	};
+
+	function onSubmitValidate(elements) {
+		let allFieldsFilled = true;
+		for(var i = 0; i < elements.length; i++){
+			var elem = $(elements[i]);
+			if(elem.attr("type") == 'email')
+				continue;
+			if(elem.val() == ""){
+				allFieldsFilled = false;
+				if(!elem.parent().hasClass("has-error")){
+					addError(elem, messages.required)
+					isFormValid = false;
+				}
+			}else{
+				elem.parent().removeClass("has-error");
+				elem.parent().find(".help-block").remove();
+			}
+		}
+		isFormValid = allFieldsFilled;
+	}
+
+	function validateElem(e) {
+		let isFieldValid = true;
+		let elem = $(this);
+		let required = elem.attr('required');
+		if((required && elem.val() == "")){
+			isFormValid = isFieldValid = false;;
+			addError(elem, messages.required);
+		} else if(elem.val() !== ""){
+			let type = elem.attr("type");
+			if(type == "email"){
+				let valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(elem.val());
+				if(!valid){
+					addError(elem, messages.email);
+					isFieldValid = false;
+				}else{
+					isFieldValid = true;
+				}
+			}
+		}
+
+		if(isFieldValid){
+			isFormValid = true;
+			elem.parent().removeClass("has-error");
+			elem.parent().find(".help-block").remove();
+		}
+	}
+
+	$.fn.bootstrapValidator = function(obj) {
+		if(!obj) return;
+		let scope = obj.scope;
+		let form = this;
+		if(obj.class){
+			let selector = "." +  obj.class;
+			var elements = $(this).find(selector);
+			elements.on('blur', validateElem);
+			$(this).on('submit', function(e){
+				onSubmitValidate(elements);
+				if(!isFormValid){
+					e.preventDefault();
+					if(obj.onInvalid){
+						obj.onInvalid();
+					}
+					return false;
+				}
+				return true;
+			});
+			let self = {
+				reValidate: function () {
+					onSubmitValidate(elements)
+				},
+				isValid: function() {
+					onSubmitValidate(elements);
+					return isFormValid;
+				}
+			};
+			return self; 
+		}
+	}
+})(jQuery);
---
+++ readme.md
@@ -0,0 +1,46 @@
+# Form validator for jQuery / Bootstrap
+
+[![N|Solid](https://shahidullahkhan.com/images/powered.png)](https://shahidullahkhan.com)
+
+[![Build Status](https://shahidullahkhan.com/images/passing.svg)](https://travis-ci.org/joemccann/dillinger)
+
+##### This is an easy to use jQuery form validator package that helps validating multiple inputs / form control with a one liner.
+
+  - Validates multiple form inputs through a single selector
+  - Validates multiple form inputs seperately
+  - Has capacity to set custom validation messages.
+  - has onSuccess & onInvalid callbacks
+
+## Installation
+
+This package requires [jQuery](https://jquery.com/) to work.
+
+Either include script through CDN:
+```javascript
+<script src="https://cdn.shahidullahkhan.com/bootstrap-form-validator.js" type="text/javascript"></script>
+```
+
+Or download it directly from here.
+[https://cdn.shahidullahkhan.com/bootstrap-form-validator.js](https://cdn.shahidullahkhan.com/bootstrap-form-validator.js)
+
+## Usage
+Initializing validator object & provider class to the validator
+```javascript
+var formValidator = $("form.validateForm").bootstrapValidator({
+				class: "validate",
+				onInvalid: function(){
+				    // do your stuff
+				}
+			});	
+```
+
+Now you can revalidate any time when you want validator to run validations again.
+
+```
+formValidator.reValidate();
+```
+
+You can check anytime if validator has passed using the following 
+```
+formValidator.isValid();
+```
